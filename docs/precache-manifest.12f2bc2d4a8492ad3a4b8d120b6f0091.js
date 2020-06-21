ed81d99c9dedd99b1f8e5c2aa617b236917fc334
Maxi <maxipg@gmail.com>
2020-06-21 19:42:28 -0300

    docs

---
+++ docs/precache-manifest.12f2bc2d4a8492ad3a4b8d120b6f0091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d66dcd7d9c437627cdcf9e28b235764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d882a8542473fed74fbe",
+    "url": "/static/css/main.42e9aa6e.chunk.css"
+  },
+  {
+    "revision": "496f528a92b3c1e745d3",
+    "url": "/static/js/2.6d4f56ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d4f56ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d882a8542473fed74fbe",
+    "url": "/static/js/main.412fe29a.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
